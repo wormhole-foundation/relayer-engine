d844bcdddf8b241a477fc63ab475843007bf6912
Joe Howarth <josephehowarth@gmail.com>
2022-10-13 11:44:45 -0500

    Make relayer-engine top-level package so projects can point at the github repo (#2)
    * Make relayer-engine top-level package so projects can point at the github repo
    
    * fix up dependencies
    
    * try postInstall
    
    * postinstall try 3
    
    * include interface build dir
    
    * yaml types
    
    * remove log tests

---
+++ relayer-plugin-interface/lib/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EnvType = void 0;
+var EnvType;
+(function (EnvType) {
+    EnvType["MAINNET"] = "MAINNET";
+    EnvType["DEVNET"] = "DEVNET";
+    EnvType["TILT"] = "TILT";
+    EnvType["LOCALHOST"] = "LOCALHOST";
+    EnvType["OTHER"] = "OTHER";
+})(EnvType = exports.EnvType || (exports.EnvType = {}));
+//# sourceMappingURL=index.js.map
---
+++ relayer/README.md
@@ -0,0 +1,9 @@
+## Relayer App
+
+# This is not ready yet, use relayer-engine as a library instead
+
+This folder will allow running the relayer engine as a stand alone application.
+
+- Install plugins packaged as npm depdendencies, optionally override their configuration, 
+- Provide the relayer engine config for each supported environment (mainnet, localhost, etc.) 
+- Run the relayer, redis and spy in a production set up
