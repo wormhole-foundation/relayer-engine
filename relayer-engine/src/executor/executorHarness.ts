874f090751689e984a0f335077c4414cc04df2df
Joe Howarth <josephehowarth@gmail.com>
2023-01-10 15:21:35 -0700

    Misc fixes from generic relayer plugin development

--- relayer-engine/src/executor/executorHarness.ts
+++ relayer-engine/src/executor/executorHarness.ts
@@ -33,7 +33,8 @@ import {
 const DEFAULT_WORKER_RESTART_MS = 10 * 1000;
 const DEFAULT_WORKER_INTERVAL_MS = 500;
 const MAX_ACTIVE_WORKFLOWS = 10;
-const SPAWN_WORKFLOW_INTERNAL = 500;
+// const SPAWN_WORKFLOW_INTERNAL = 500;
+const SPAWN_WORKFLOW_INTERNAL = 2000;
 
 export interface WorkerInfo {
   id: number;
