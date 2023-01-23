8afd8c6a3d9feabfcf3052b05f6b707cf7121ea7
Joe Howarth <josephehowarth@gmail.com>
2023-01-23 12:21:06 -0700

    save

--- relayer-engine/src/listener/eventHarness.ts
+++ relayer-engine/src/listener/eventHarness.ts
@@ -1,13 +1,25 @@
-import { SignedVaa } from "@certusone/wormhole-sdk";
-import { Plugin, Providers } from "relayer-plugin-interface";
+import { ChainId, SignedVaa } from "@certusone/wormhole-sdk";
+import * as wh from "@certusone/wormhole-sdk";
+import {
+  ParsedVaaWithBytes,
+  Plugin,
+  Providers,
+} from "relayer-plugin-interface";
 import { getScopedLogger, ScopedLogger } from "../helpers/logHelper";
 import { Storage } from "../storage";
-import { parseVaaWithBytes } from "../utils/utils";
+import {
+  assertChainId,
+  parseVaaWithBytes,
+  wormholeBytesToHex,
+} from "../utils/utils";
 import {
   createdWorkflowsCounter,
   erroredEventsCounter,
   receivedEventsCounter,
 } from "./metrics";
+import * as grpcWebNodeHttpTransport from "@improbable-eng/grpc-web-node-http-transport";
+
+const wormholeRpc = "https://wormhole-v2-testnet-api.certus.one";
 
 let _logger: ScopedLogger;
 const logger = () => {
@@ -17,6 +29,39 @@ const logger = () => {
   return _logger;
 };
 
+async function fetchMissedVaas(
+  plugin: Plugin,
+  storage: Storage,
+  providers: Providers,
+  chainId: ChainId,
+  emitterAddress: string,
+  lastSeenSequence: number,
+  latestSequence: number,
+): Promise<void> {
+  for (let seq = lastSeenSequence + 1; seq < latestSequence; ++seq) {
+    try {
+      const resp = await wh.getSignedVAAWithRetry(
+        [wormholeRpc],
+        chainId,
+        emitterAddress,
+        seq.toString(),
+        { transport: grpcWebNodeHttpTransport.NodeHttpTransport() },
+      );
+
+      if (!resp?.vaaBytes) {
+        logger().debug(
+          `Attempted to fetch vaa with lagging sequence number but not found in wormhole rpc. ${chainId}:${emitterAddress}:${seq}`,
+        );
+        continue;
+      }
+      consumeEventHarnessInner(resp.vaaBytes, plugin, storage, providers);
+    } catch (e) {
+      logger().error("Attempted to fetch missed Vaa but encountered error");
+      logger().error(e);
+    }
+  }
+}
+
 export async function consumeEventHarness(
   vaa: SignedVaa,
   plugin: Plugin,
@@ -24,9 +69,35 @@ export async function consumeEventHarness(
   providers: Providers,
   extraData?: any[],
 ): Promise<void> {
+  const parsedVaa = parseVaaWithBytes(vaa);
+
+  const chainId = assertChainId(parsedVaa.emitterChain);
+  const emitterAddress = wormholeBytesToHex(parsedVaa.emitterAddress);
+  const emitterRecord = await storage.getEmitterRecord(chainId, emitterAddress);
+
+  if (emitterRecord) {
+    await fetchMissedVaas(
+      plugin,
+      storage,
+      providers,
+      chainId,
+      emitterAddress,
+      emitterRecord.lastSeenSequence,
+      Number(parsedVaa.sequence),
+    );
+  }
+  await consumeEventHarnessInner(vaa, plugin, storage, providers, extraData);
+}
+
+export async function consumeEventHarnessInner(
+  vaa: SignedVaa | ParsedVaaWithBytes,
+  plugin: Plugin,
+  storage: Storage,
+  providers: Providers,
+  extraData?: any[],
+): Promise<void> {
   try {
     receivedEventsCounter.labels({ plugin: plugin.pluginName }).inc();
-
     const parsedVaa = parseVaaWithBytes(vaa);
     const { workflowData, workflowOptions } = await plugin.consumeEvent(
       parsedVaa,
@@ -44,6 +115,9 @@ export async function consumeEventHarness(
       });
       createdWorkflowsCounter.labels({ plugin: plugin.pluginName }).inc();
     }
+    const chainId = assertChainId(parsedVaa.emitterChain);
+    const emitterAddress = wormholeBytesToHex(parsedVaa.emitterAddress);
+    storage.setEmitterRecord(chainId, emitterAddress, );
   } catch (e) {
     const l = logger();
     l.error(`Encountered error consumingEvent for plugin ${plugin.pluginName}`);
