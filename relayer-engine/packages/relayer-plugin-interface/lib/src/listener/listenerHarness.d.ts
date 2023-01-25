481240f2f2af2f200d2ab4a458beeaa9744384b2
Joe Howarth <josephehowarth@gmail.com>
2023-01-25 11:16:34 -0700

    Fix: remove engine lib files in interface package

--- relayer-engine/packages/relayer-plugin-interface/lib/src/listener/listenerHarness.d.ts
+++
@@ -1,3 +0,0 @@
-import { Plugin } from "relayer-plugin-interface";
-import { Storage } from "../storage";
-export declare function run(plugins: Plugin[], storage: Storage): Promise<void>;
--- relayer-engine/packages/relayer-plugin-interface/lib/src/listener/pluginEventSource.d.ts
+++
@@ -1,8 +0,0 @@
-import { EventSource, Plugin, Providers, Storage } from "..";
-export declare class PluginEventSource {
-    readonly storage: Storage;
-    readonly providers: Providers;
-    plugins: Map<string, Plugin>;
-    constructor(storage: Storage, plugins: Plugin[], providers: Providers);
-    getEventSourceFn(pluginName: string): EventSource;
-}
