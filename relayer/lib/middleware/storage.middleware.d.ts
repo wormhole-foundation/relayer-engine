35d2d232f731df57234b7ca73443eab4eb3cb462
Gabriel Zimmermann <gabrielemanuel@gmail.com>
2023-03-16 01:11:42 -0300

    refactor to install from github

--- relayer/lib/middleware/storage.middleware.d.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from "../context";
-import { Middleware } from "../compose.middleware";
-interface StorageConfig {
-    redis: {
-        url: string;
-    };
-}
-export declare function storage(cfg: StorageConfig): Middleware<Context>;
-export {};
