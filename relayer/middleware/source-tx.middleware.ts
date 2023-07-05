45ecae5c1f54a57d739c41f45e1e2a39d98d7c3d
scnale <sebinale@gmail.com>
2023-07-05 18:46:19 -0300

    relayer: improves error messages in source tx middleware. (#160)
    * relayer: improves error messages in source tx middleware.
    
    In the fetch function:
    - Adds HTTP body response to some errors if present (still assumes it is UTF8 encoded)
    - Throws error on anything outside the 200-299 HTTP response status range.
    
    * relayer: Simplifies HTTP body loggging.

--- relayer/middleware/source-tx.middleware.ts
+++ relayer/middleware/source-tx.middleware.ts
@@ -59,7 +59,7 @@ export function sourceTx(
     );
     ctx.logger?.debug(
       txHash === ""
-        ? "Could not retrive tx hash."
+        ? "Could not retrieve tx hash."
         : `Retrieved tx hash: ${txHash}`,
     );
     ctx.sourceTxHash = txHash;
@@ -79,6 +79,7 @@ export async function fetchVaaHash(
   let attempt = 0;
   let txHash = "";
   do {
+    let body;
     try {
       const res = await fetch(
         `${baseEndpoint}/api/v1/vaas/${emitterChain}/${emitterAddress.toString(
@@ -87,15 +88,22 @@ export async function fetchVaaHash(
       );
       if (res.status === 404) {
         throw new Error("Not found yet.");
-      } else if (res.status > 500) {
-        throw new Error(`Got: ${res.status}`);
       }
-      txHash = (await res.json()).data?.txHash;
-    } catch (e) {
-      logger?.error(
-        `could not obtain txHash, attempt: ${attempt} of ${retries}.`,
-        e,
-      );
+
+      // Note that we're assuming the response is UTF8 encoded here.
+      body = await res.text();
+      if (!res.ok) {
+        throw new Error(`Unexpected HTTP response. Status: ${res.status}`);
+      }
+
+      // TODO: consider restricting this code path further to just status 200 and a few others.
+      txHash = JSON.parse(body).data?.txHash;
+    } catch (error) {
+      const httpBodyText =
+        body !== undefined ? `\nHTTP response body: ${body}` : "";
+      const errorMessage = `could not obtain txHash, attempt: ${attempt} of ${retries}.${httpBodyText}
+Error: ${error}`;
+      logger?.error(errorMessage);
       await sleep((attempt + 1) * 200); // linear wait
     }
   } while (++attempt < retries && !txHash);
