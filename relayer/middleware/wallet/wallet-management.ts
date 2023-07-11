a9d3c620462aef7a441c875b19a6c57908aa6d2b
solanoe <solanoepalacio@gmail.com>
2023-07-11 11:57:47 -0300

    un-silence wallet-management

--- relayer/middleware/wallet/wallet-management.ts
+++ relayer/middleware/wallet/wallet-management.ts
@@ -139,8 +139,7 @@ export function startWalletManagement(
     config: wallets,
     options: {
       failOnInvalidChain: false,
-      failOnInvalidTokens: false,
-      // logger: logger?.child({ module: "wallet-manager" }),
+      logger: logger?.child({ module: "wallet-manager" }),
       logLevel: "error",
       metrics: metricsOpts,
     },
