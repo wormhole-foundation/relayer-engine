e0bdb7f8699b561389b700a21b78e06cb6de528a
solanoe <solanoepalacio@gmail.com>
2023-07-11 11:37:39 -0300

    missed vaa basic implementation

--- relayer/storage/redis-storage.ts
+++ relayer/storage/redis-storage.ts
@@ -116,9 +116,9 @@ export class RedisStorage implements Storage {
   }
 
   getPrefix() {
-    return this.prefix;
+    return [this.prefix, this.opts.queueName].join(":");
   }
-  
+
   async addVaaToQueue(vaaBytes: Buffer): Promise<RelayJob> {
     const parsedVaa = parseVaa(vaaBytes);
     const id = this.vaaId(parsedVaa);
