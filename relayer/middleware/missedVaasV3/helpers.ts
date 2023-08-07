a7763be5ebdbf4080db32d85f65aef6cee2a123f
Gabriel Zimmermann <gabrielemanuel@gmail.com>
2023-08-07 13:03:42 -0300

    Run prettier

--- relayer/middleware/missedVaasV3/helpers.ts
+++ relayer/middleware/missedVaasV3/helpers.ts
@@ -3,49 +3,56 @@ import { FilterIdentifier } from "./worker";
 import { MissedVaaMetrics } from "./metrics";
 
 export type MissedVaaRunStats = {
-  processed: string[],
-  seenSequences: string[],
-  failedToRecover: string[],
-  failedToReprocess: string[],
-  lookAheadSequences: string[],
-  missingSequences: string[],
+  processed: string[];
+  seenSequences: string[];
+  failedToRecover: string[];
+  failedToReprocess: string[];
+  lookAheadSequences: string[];
+  missingSequences: string[];
 };
 
 export type SequenceStats = {
-  lastSafeSequence: number,
-  lastSeenSequence: number,
-  firstSeenSequence: number,
+  lastSafeSequence: number;
+  lastSeenSequence: number;
+  firstSeenSequence: number;
 };
 
 export function calculateSequenceStats(
   runStats: MissedVaaRunStats,
   failedToFetchSequences: string[],
-  previousSafeSequence?: string
+  previousSafeSequence?: string,
 ): SequenceStats {
   const { seenSequences } = runStats;
 
-  const lastSafeSequence = calculateLastSafeSequence(runStats, failedToFetchSequences, previousSafeSequence);
-  const lastSeenSequence = Math.max(Number(seenSequences[seenSequences.length - 1]), lastSafeSequence);
+  const lastSafeSequence = calculateLastSafeSequence(
+    runStats,
+    failedToFetchSequences,
+    previousSafeSequence,
+  );
+  const lastSeenSequence = Math.max(
+    Number(seenSequences[seenSequences.length - 1]),
+    lastSafeSequence,
+  );
   const firstSeenSequence = Number(seenSequences[0]) || 0;
 
-  return { lastSafeSequence, lastSeenSequence, firstSeenSequence}
+  return { lastSafeSequence, lastSeenSequence, firstSeenSequence };
 }
 
-function calculateLastSafeSequence (
+function calculateLastSafeSequence(
   runStats: MissedVaaRunStats,
   failedToFetchSequences: string[],
-  previousSafeSequence?: string
+  previousSafeSequence?: string,
 ): number {
   if (failedToFetchSequences && failedToFetchSequences.length > 0) {
     // we have sequences that we have failed to update before. We won't update the last
     // safe sequence. Return the the last sequence before the first one we failed to fetch.
     return Number(failedToFetchSequences[0]) - 1;
-  };
+  }
 
   const { missingSequences, failedToRecover, failedToReprocess } = runStats;
 
-  const missingSequencesFailedToReprocess = failedToRecover.length === 0
-    && failedToReprocess.length === 0;
+  const missingSequencesFailedToReprocess =
+    failedToRecover.length === 0 && failedToReprocess.length === 0;
 
   if (missingSequences.length > 0 && missingSequencesFailedToReprocess) {
     // we found some missing sequences on this run, but we were able to reprocess them
@@ -56,13 +63,16 @@ function calculateLastSafeSequence (
   // No missing sequences up to seenSequences
   // if the there was vaas recovered by the lookahead, use that as the
   // last safe sequence. Otherwise, use the last seen sequence.
-  const lastSeenSequence = runStats.lookAheadSequences.length > 0
-    ? runStats.lookAheadSequences[runStats.lookAheadSequences.length - 1]
-    : runStats.seenSequences[runStats.seenSequences.length - 1];
+  const lastSeenSequence =
+    runStats.lookAheadSequences.length > 0
+      ? runStats.lookAheadSequences[runStats.lookAheadSequences.length - 1]
+      : runStats.seenSequences[runStats.seenSequences.length - 1];
 
   return lastSeenSequence
     ? Number(lastSeenSequence)
-    : previousSafeSequence ? Number(previousSafeSequence) : 0;
+    : previousSafeSequence
+    ? Number(previousSafeSequence)
+    : 0;
 }
 
 export function updateMetrics(
@@ -70,7 +80,7 @@ export function updateMetrics(
   filter: FilterIdentifier,
   missedVaas: MissedVaaRunStats,
   sequenceStats: SequenceStats,
-  ) {
+) {
   const { emitterChain, emitterAddress } = filter;
   const vaasProcessed = missedVaas.processed.length;
   // This are VAAs that were found missing between known sequences, but we failed
@@ -78,10 +88,14 @@ export function updateMetrics(
   const vaasFailedToRecover = missedVaas.failedToRecover.length;
   // This are VAAs that were found but failed when trying to re-queue them
   const vaasFailedToReprocess = missedVaas.failedToReprocess.length;
-  const vaasFound = missedVaas.missingSequences.length + missedVaas.lookAheadSequences.length;
+  const vaasFound =
+    missedVaas.missingSequences.length + missedVaas.lookAheadSequences.length;
 
-  const labels = { emitterChain: coalesceChainName(emitterChain as ChainId), emitterAddress };
-  
+  const labels = {
+    emitterChain: coalesceChainName(emitterChain as ChainId),
+    emitterAddress,
+  };
+
   if (vaasFound > 0) {
     metrics.detectedVaas?.labels(labels).inc(vaasFound);
   }
@@ -98,10 +112,13 @@ export function updateMetrics(
     metrics.failedToReprocess?.labels(labels).inc(vaasFailedToReprocess);
   }
 
-  const { lastSeenSequence, lastSafeSequence, firstSeenSequence } = sequenceStats;
+  const { lastSeenSequence, lastSafeSequence, firstSeenSequence } =
+    sequenceStats;
 
   metrics.lastSeenSequence?.labels(labels).set(lastSeenSequence);
   metrics.lastSafeSequence?.labels(labels).set(lastSafeSequence);
   metrics.firstSeenSequence?.labels(labels).set(firstSeenSequence);
-  metrics.missingSequences?.labels(labels).set(missedVaas.missingSequences.length ? 1 : 0);
+  metrics.missingSequences
+    ?.labels(labels)
+    .set(missedVaas.missingSequences.length ? 1 : 0);
 }
