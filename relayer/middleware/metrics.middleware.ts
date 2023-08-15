faddba158e514245b8f9031566cd2275b12287ac
matias martinez <matias@xlabs.xyz>
2023-08-15 08:49:10 -0300

    replacement for storage max failures reached counter

--- relayer/middleware/metrics.middleware.ts
+++ relayer/middleware/metrics.middleware.ts
@@ -3,7 +3,6 @@ import { Middleware } from "../compose.middleware";
 import { StorageContext } from "../storage/storage";
 import { Counter, Registry } from "prom-client";
 
-
 type MetricRecord = Record<string, string | number>;
 
 export interface MetricsOpts<C extends StorageContext> {
@@ -16,84 +15,101 @@ export class MetricLabelsOpts<C extends StorageContext> {
   labelNames: string[];
   customizer: (context: C) => Promise<MetricRecord>;
 
-  constructor(labelNames: string[] = [], customizer: (ctx: C) => Promise<MetricRecord>) {
+  constructor(
+    labelNames: string[] = [],
+    customizer: (ctx: C) => Promise<MetricRecord>,
+  ) {
     this.labelNames = labelNames;
     this.customizer = customizer;
   }
 }
 
+const getLabels = async <C extends StorageContext>(
+  ctx: C,
+  opts: MetricsOpts<C>,
+  failed: boolean,
+) => {
+  const defaultLabels = { status: failed ? "failed" : "succeeded" };
+
+  try {
+    const labels: MetricRecord = await (opts.labels?.customizer(ctx) ??
+      Promise.resolve({}));
+
+    const allowedKeys = opts.labels?.labelNames ?? [];
+    const validatedLabels: typeof labels = {};
+    allowedKeys.forEach(key => {
+      if (labels[key]) {
+        validatedLabels[key] = labels[key];
+      }
+    });
+
+    return { ...validatedLabels, ...defaultLabels };
+  } catch (error) {
+    ctx.logger?.error(
+      "Failed to customize metric labels, please review",
+      error,
+    );
+
+    return defaultLabels;
+  }
+};
+
 export function metrics<C extends StorageContext>(
-  opts: MetricsOpts<C> = { },
+  opts: MetricsOpts<C> = {},
 ): Middleware<C> {
   opts.registry = opts.registry || register;
-
-
-  const getLabels = async (ctx: C, failed: boolean) => {
-    const defaultLabels = { status: failed ? "failed" : "succeeded" };
-
-    try {
-      const labels: MetricRecord = await (opts.labels?.customizer(ctx) ?? Promise.resolve({}));
-
-      const allowedKeys = opts.labels?.labelNames ?? [];
-      const validatedLabels: typeof labels = {};
-      allowedKeys.forEach(key => {
-        if (labels[key]) {
-          validatedLabels[key] = labels[key];
-        }
-      });
-
-      return { ...validatedLabels, ...defaultLabels };
-    } catch (error) {
-      ctx.logger?.error("Failed to customize metric labels, please review", error);
-
-      return defaultLabels;
-    }
-  }
 
   const processedVaasTotal = new Counter({
     name: "vaas_processed_total",
     help: "Number of incoming vaas processed successfully or unsuccessfully.",
-    labelNames: opts.labels?.labelNames ?? [],
+    labelNames: ["status"].concat(opts.labels?.labelNames ?? []),
     registers: [opts.registry],
   });
 
   const finishedVaasTotal = new Counter({
     name: "vaas_finished_total",
     help: "Number of vaas processed successfully or unsuccessfully.",
-    labelNames: ["status"].concat(opts.labels?.labelNames ?? []),
+    labelNames: ["status", "terminal"].concat(opts.labels?.labelNames ?? []),
     registers: [opts.registry],
   });
 
   const processingDuration = new Histogram({
-    name: `vaas_processing_duration`,
+    name: "vaas_processing_duration",
     help: "Processing time in ms for jobs",
-    buckets: opts.processingTimeBuckets ?? [6000, 7000, 7500, 8000, 8500, 9000, 10000, 12000],
+    buckets: opts.processingTimeBuckets ?? [
+      6000, 7000, 7500, 8000, 8500, 9000, 10000, 12000,
+    ],
     labelNames: ["status"].concat(opts.labels?.labelNames ?? []),
     registers: [opts.registry],
   });
 
   return async (ctx: C, next) => {
+    processedVaasTotal.inc();
+
     const startTime = Date.now();
     let failure: Error = null;
-    processedVaasTotal.inc();
 
     try {
       await next();
     } catch (e) {
-      failure = e; // TODO: test failure case
+      failure = e;
     }
 
     const time = Date.now() - startTime;
-    const labels = await getLabels(ctx, failure !== null);
+    const labels = await getLabels(ctx, opts, failure !== null);
+    processingDuration.labels(labels).observe(time);
 
     if (failure) {
-      processingDuration.labels(labels).observe(time);
-      finishedVaasTotal.labels(labels).inc();
+      const reachedMaxAttempts =
+        (ctx.storage.job?.attempts ?? 0) ===
+        (ctx.storage.job?.maxAttempts ?? -1);
+      finishedVaasTotal
+        .labels({ ...labels, terminal: `${reachedMaxAttempts}` })
+        .inc();
 
-      throw failure
+      throw failure;
     }
 
-    processingDuration.labels(labels).observe(time);
     finishedVaasTotal.labels(labels).inc();
   };
 }
--- test/middleware/metrics.middleware.test.ts
+++ test/middleware/metrics.middleware.test.ts
@@ -1,10 +1,10 @@
-import {
-    beforeEach,
-    describe,
-    test
-} from "@jest/globals";
+import { beforeEach, describe, test } from "@jest/globals";
 import { Registry } from "prom-client";
-import { MetricLabelsOpts, metrics, MetricsOpts } from "../../relayer/middleware/metrics.middleware";
+import {
+  MetricLabelsOpts,
+  metrics,
+  MetricsOpts,
+} from "../../relayer/middleware/metrics.middleware";
 import { Middleware, Next } from "../../relayer/compose.middleware";
 import { parseVaa } from "@certusone/wormhole-sdk";
 import { StorageContext } from "../../relayer/storage/storage";
@@ -13,9 +13,11 @@ import { Environment } from "../../relayer/environment";
 
 type TestContext = StorageContext & { target?: string };
 
-const vaa = Buffer.from("AQAAAAABAGYGQ1g8mB5UMkeq28zodCdhDUk8YSjRSseFmP3VkKHMDUuZmDpQ6ccsPSx+bUkDIDp+ud6Qfes9nvZcWHkH1tQAZNPDWAg9AQAAAgAAAAAAAAAAAAAAAPiQmC+TEN9X0A9lnPT9h+Za3tjXAAAAAAACh1YBAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPoAAAAAAAAAAAAAAAAtPvycRQ/T797kaXe0xgF5CsiCNYAAgAAAAAAAAAAAAAAAI8moAJdzMbPwHp9OHVigKEOKVrXAB4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==", "base64");
-
-const nextProvider: (implementation?: () => void) => Next = (implementation = () => { }) => jest.fn().mockImplementation(() => Promise.resolve(implementation()));
+const vaa = Buffer.from(
+  "AQAAAAABAGYGQ1g8mB5UMkeq28zodCdhDUk8YSjRSseFmP3VkKHMDUuZmDpQ6ccsPSx+bUkDIDp+ud6Qfes9nvZcWHkH1tQAZNPDWAg9AQAAAgAAAAAAAAAAAAAAAPiQmC+TEN9X0A9lnPT9h+Za3tjXAAAAAAACh1YBAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPoAAAAAAAAAAAAAAAAtPvycRQ/T797kaXe0xgF5CsiCNYAAgAAAAAAAAAAAAAAAI8moAJdzMbPwHp9OHVigKEOKVrXAB4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
+  "base64",
+);
+const targetLabel = "targetLabel";
 
 let ctx: TestContext;
 let middleware: Middleware<TestContext> | null;
@@ -24,99 +26,163 @@ let labelOpts: MetricLabelsOpts<TestContext>;
 let next: Next;
 
 describe("metrics middleware", () => {
+  beforeEach(() => {
+    registry.clear();
+    middleware = null;
+  });
 
-    beforeEach(() => {
-        registry.clear();
-        middleware = null;
+  test("should expose known metrics", async () => {
+    givenAMetricsMiddleware();
+    givenAContext();
+    const next = nextProvider();
+
+    await whenExecuted(next);
+
+    thenNextCalled(next);
+    await thenMetricPresent("vaas_processed_total", val => expect(val).toBe(1));
+    await thenMetricPresent(
+      "vaas_finished_total",
+      val => expect(val).toBe(1),
+      true,
+    );
+  });
+
+  test("should expose time histogram", async () => {
+    givenAMetricsMiddleware();
+    givenAContext();
+
+    await whenExecuted(nextProvider());
+
+    await thenMetricPresent(
+      "vaas_processing_duration",
+      val => expect(val).toBeGreaterThan(0),
+      true,
+    );
+  });
+
+  test("should allow to customize labels", async () => {
+    const expectedTarget = "base";
+    givenAMetricsMiddleware({
+      labels: { labelNames: [targetLabel], customizer: targetCustomizer },
     });
+    givenAContext();
 
-    test("should expose known metrics", async () => {
-        givenAMetricsMiddleware();
-        givenAContext();
-        const next = nextProvider();
+    await whenExecuted(
+      nextProvider(() => {
+        ctx.target = expectedTarget;
+      }),
+    );
 
-        await whenExecuted(next);
+    await thenMetricPresent(
+      "vaas_processing_duration",
+      (val, labels) => {
+        expect(val).toBeGreaterThan(0);
+        expect(labels[targetLabel]).toBe(expectedTarget);
+      },
+      true,
+    );
+  });
 
-        thenNextCalled(next);
-        await thenMetricPresent("vaas_processed_total", (val) => expect(val).toBe(1));
-        await thenMetricPresent("vaas_finished_total", (val) => expect(val).toBe(1), true);
+  test("should measure failures", async () => {
+    const expectedTarget = "celo";
+    givenAMetricsMiddleware({
+      labels: { labelNames: [targetLabel], customizer: targetCustomizer },
     });
+    givenAContext();
 
-    test("should expose time histogram", async () => {
-        givenAMetricsMiddleware();
-        givenAContext();
+    await whenExecuted(
+      nextProvider(() => {
+        ctx.target = expectedTarget;
+        throw new Error("runtime failure");
+      }),
+    );
 
-        await whenExecuted(nextProvider());
-
-        await thenMetricPresent("vaas_processing_duration", (val) => expect(val).toBeGreaterThan(0), true);
-    });
-
-    test("should allow to customize labels", async () => {
-        const expectedTarget = "base";
-        const labelName = "targetLabel";
-        givenAMetricsMiddleware({ labels: { labelNames: [labelName], customizer: targetCustomizer } });
-        givenAContext();
-
-        await whenExecuted(nextProvider(() => { ctx.target = expectedTarget; }));
-
-        await thenMetricPresent(
-            "vaas_processing_duration",
-            (val, labels) => {
-                expect(val).toBeGreaterThan(0);
-                expect(labels[labelName]).toBe(expectedTarget);
-            },
-            true
-        );
-    });
+    await thenMetricPresent(
+      "vaas_finished_total",
+      (val, labels) => {
+        expect(val).toBe(1);
+        expect(labels.status).toBe("failed");
+        expect(labels.terminal).toBe("true");
+        expect(labels[targetLabel]).toBe(expectedTarget);
+      },
+      false,
+    );
+    await thenMetricPresent(
+      "vaas_processing_duration",
+      (val, labels) => {
+        expect(val).toBeGreaterThan(0);
+        expect(labels.status).toBe("failed");
+        expect(labels[targetLabel]).toBe(expectedTarget);
+      },
+      false,
+    );
+  });
 });
 
 const createContext = () => ({
-    storage: { job: createRelayJob() },
-    locals: {},
-    fetchVaa: () => Promise.resolve({} as ParsedVaaWithBytes),
-    fetchVaas: () => Promise.resolve([]),
-    processVaa: () => Promise.resolve(),
-    config: { spyFilters: [{}] },
-    env: Environment.DEVNET,
-    on: () => { }
+  storage: { job: createRelayJob() },
+  locals: {},
+  fetchVaa: () => Promise.resolve({} as ParsedVaaWithBytes),
+  fetchVaas: () => Promise.resolve([]),
+  processVaa: () => Promise.resolve(),
+  config: { spyFilters: [{}] },
+  env: Environment.DEVNET,
+  on: () => {},
 });
 
-const targetCustomizer = (ctx: TestContext) => Promise.resolve({ targetLabel: ctx.target ?? 0 });
+const targetCustomizer = (ctx: TestContext) =>
+  Promise.resolve({ targetLabel: ctx.target ?? 0 });
 
 const createRelayJob = () => ({
-    id: "id",
-    name: "name",
-    data: {
-        vaaBytes: vaa,
-        parsedVaa: parseVaa(vaa)
-    },
-    attempts: 0,
-    maxAttempts: 1,
-    log: (logRow: string) => Promise.resolve(5),
-    updateProgress: (progress: number | object) => Promise.resolve()
+  id: "id",
+  name: "name",
+  data: {
+    vaaBytes: vaa,
+    parsedVaa: parseVaa(vaa),
+  },
+  attempts: 1,
+  maxAttempts: 1,
+  log: (logRow: string) => Promise.resolve(5),
+  updateProgress: (progress: number | object) => Promise.resolve(),
 });
 
+const nextProvider: (implementation?: () => void) => Next = (
+  implementation = () => {},
+) => jest.fn().mockImplementation(() => Promise.resolve(implementation()));
+
 const givenAMetricsMiddleware = (opts: MetricsOpts<TestContext> = {}) => {
-    middleware = metrics<TestContext>({ registry, ...opts })
+  middleware = metrics<TestContext>({ registry, ...opts });
 };
 
-const givenAContext = () => ctx = createContext();
+const givenAContext = () => (ctx = createContext());
 
-const whenExecuted = async (next: Next) => await middleware!(ctx, next);
+const whenExecuted = async (next: Next) => {
+  try {
+    await middleware!(ctx, next);
+  } catch (error) {
+    // ignoring it
+  }
+};
 
 const thenMetricPresent = async (
-    metric: string,
-    expectation: (value: any, labels: Partial<Record<string, string | number>>) => void,
-    withStatus: boolean = false) => {
-    const metricValue = (await registry.getSingleMetric(metric)!.get()).values[0];
-    expect(metricValue).toBeDefined();
-    expectation(metricValue.value, metricValue.labels);
-    if (withStatus) {
-        expect(metricValue.labels["status"]).toBeDefined();
-    }
-    labelOpts?.labelNames!.forEach(labelName => expect(metricValue.labels[labelName]).toBeDefined());
+  metric: string,
+  expectation: (
+    value: any,
+    labels: Partial<Record<string, string | number>>,
+  ) => void,
+  withStatus: boolean = false,
+) => {
+  const metricValue = (await registry.getSingleMetric(metric)!.get()).values[0];
+  expect(metricValue).toBeDefined();
+  expectation(metricValue.value, metricValue.labels);
+  if (withStatus) {
+    expect(metricValue.labels["status"]).toBeDefined();
+  }
+  labelOpts?.labelNames!.forEach(labelName =>
+    expect(metricValue.labels[labelName]).toBeDefined(),
+  );
 };
 
 const thenNextCalled = (next: Next) => {
-    expect(next).toHaveBeenCalledTimes(1);
+  expect(next).toHaveBeenCalledTimes(1);
 };
