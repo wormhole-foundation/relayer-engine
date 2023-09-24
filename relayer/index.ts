6d2fec744b8dec4fe624e7a56730723c31fbbff5
Gabriel Zimmermann <gabrielemanuel@gmail.com>
2023-09-24 16:08:02 -0300

    Exports redist storage improvements

--- relayer/index.ts
+++ relayer/index.ts
@@ -3,7 +3,7 @@ export * from "./environment.js";
 export * from "./application-standard.js";
 export * from "./context.js";
 export * from "./compose.middleware.js";
-export * from "./storage/storage.js";
+export * from "./storage/index.js";
 export * from "./logging.js";
 export * from "./middleware/index.js";
 export * from "./utils.js";
